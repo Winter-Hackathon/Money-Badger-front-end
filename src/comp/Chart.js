6b4f373ba9f1fdb176a86bdf8bd41ce76afa5c22
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-26 22:56:54 -0600

    charts is working

--- src/comp/Chart.js
+++ src/comp/Chart.js
@@ -1,58 +1,99 @@
-import React, { Component } from 'react';
-import CanvasJSReact from '../assets/canvasjs.react';
+import React, { Component } from "react";
+import CanvasJSReact from "../assets/canvasjs.react";
+import axios from "axios";
+import { object } from "prop-types";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
- 
+
 class CandlestickChart extends Component {
-	render() {
-		const options = {
-			theme: "light2", // "light1", "light2", "dark1", "dark2"
-			animationEnabled: true,
-			exportEnabled: true,
-			title:{
-				text: "Intel Corporation Stock Price -  2017"
-			},
-			axisX: {
-				valueFormatString: "MMM"
-			},
-			axisY: {
-				includeZero:false,
-				prefix: "$",
-				title: "Price (in USD)"
-			},
-			data: [{
-				type: "candlestick",
-				showInLegend: true,
-				name: "Intel Corporation",
-				yValueFormatString: "$###0.00",
-				xValueFormatString: "MMMM YY",
-				dataPoints: [
-					{ x: new Date("2017-01-01"), y: [36.61, 38.45, 36.19, 36.82] },
-					{ x: new Date("2017-02-01"), y: [36.82, 36.95, 34.84, 36.20] },
-					{ x: new Date("2017-03-01"), y: [35.85, 36.30, 34.66, 36.07] },
-					{ x: new Date("2017-04-01"), y: [36.19, 37.50, 35.21, 36.15] },
-					{ x: new Date("2017-05-01"), y: [36.11, 37.17, 35.02, 36.11] },
-					{ x: new Date("2017-06-01"), y: [36.12, 36.57, 33.34, 33.74] },
-					{ x: new Date("2017-07-01"), y: [33.51, 35.86, 33.23, 35.47] },
-					{ x: new Date("2017-08-01"), y: [35.66, 36.70, 34.38, 35.07] },
-					{ x: new Date("2017-09-01"), y: [35.24, 38.15, 34.93, 38.08] },
-					{ x: new Date("2017-10-01"), y: [38.12, 45.80, 38.08, 45.49] },
-					{ x: new Date("2017-11-01"), y: [45.97, 47.30, 43.77, 44.84] },
-					{ x: new Date("2017-12-01"), y: [44.73, 47.64, 42.67, 46.16] }
-				]
-			}
-		  ]
-		}
-		
-		return (
-		<div>
-			<h1>React Candlestick Chart</h1>
-			<CanvasJSChart options = {options} 
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.TimeSeries = "TIME_SERIES_WEEKLY";
+    this.dataPoints = [];
+    this.ourValues = [];
+    this.count = 0;
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://www.alphavantage.co/query?function=${this.TimeSeries}&symbol=TSLA&interval=5min&apikey=ACT64CZ1Z26W5JYR`
+      )
+      .then(res => {
+        console.log("RES: ", res);
+        // console.log("TIMESERIES: ", res.data["Time Series (Daily)"]);
+        for (let property in res.data["Weekly Time Series"]) {
+          //   console.log("Property: ", property);
+          //   console.log(
+          //     "VALUES: ",
+          //     Object.values(res.data["Time Series (Daily)"][property])
+          //   );
+          //   console.log("Y: ", [
+          //     Number.parseFloat(this.ourValues[0]),
+          //     Number.parseFloat(this.ourValues[1]),
+          //     Number.parseFloat(this.ourValues[2]),
+          //     Number.parseFloat(this.ourValues[3])
+          //   ]);
+          this.ourValues = Object.values(
+            res.data["Weekly Time Series"][property]
+          );
+          //   console.log("OURVALUES: ", this.ourValues);
+          if (this.count < 100) {
+            this.dataPoints.push({
+              x: new Date(`${property}`),
+              y: [
+                Number.parseFloat(this.ourValues[0]),
+                Number.parseFloat(this.ourValues[1]),
+                Number.parseFloat(this.ourValues[2]),
+                Number.parseFloat(this.ourValues[3])
+              ]
+            });
+            this.count++;
+          }
+          this.chart.render();
+        }
+      })
+      .catch(err => {
+        console.log("ERROR: ", err);
+      });
+  }
+
+  render() {
+    const options = {
+      theme: "light2", // "light1", "light2", "dark1", "dark2"
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Tesla Corporation Stock Price -  2017"
+      },
+      axisX: {
+        valueFormatString: "MMM"
+      },
+      axisY: {
+        includeZero: false,
+        prefix: "$",
+        title: "Price (in USD)"
+      },
+      data: [
+        {
+          type: "candlestick",
+          showInLegend: true,
+          name: "Tesla Corporation",
+          yValueFormatString: "$###0.00",
+          xValueFormatString: "MMMM YY",
+          dataPoints: this.dataPoints
+        }
+      ]
+    };
+    console.log("IN RENDER: ", options);
+    return (
+      <div>
+        <h1>React Candlestick Chart</h1>
+        <CanvasJSChart options={options} onRef={ref => (this.chart = ref)} />
+        {console.log("DATA POINTS: ", this.dataPoints)}
+        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+      </div>
+    );
+  }
 }
 
 export default CandlestickChart;
